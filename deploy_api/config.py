c61cb6dca5d63210908ab3e0f6a6dd3fdf11d392
vergnetp <vergnetp@yahoo.fr>
2026-01-22 09:52:37 +0000

    flask n bug fix

--- deploy_api/config.py
+++ deploy_api/config.py
@@ -17,7 +17,7 @@ Note: Loaded here for worker.py, and also by create_service() for the API.
 # =============================================================================
 # BUILD TIMESTAMP - Auto-generated when Claude creates deploy_api.zip
 # =============================================================================
-DEPLOY_API_VERSION = "2026-01-22 06:52 UTC"
+DEPLOY_API_VERSION = "2026-01-22 09:44 UTC"
 # =============================================================================
 
 import os
