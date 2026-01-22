db979c0ff2378feafc6ae1166dd05b1e5644da6d
vergnetp <vergnetp@yahoo.fr>
2026-01-22 06:55:41 +0000

    duplciated ping routes bug fix

--- deploy_api/config.py
+++ deploy_api/config.py
@@ -17,7 +17,7 @@ Note: Loaded here for worker.py, and also by create_service() for the API.
 # =============================================================================
 # BUILD TIMESTAMP - Auto-generated when Claude creates deploy_api.zip
 # =============================================================================
-DEPLOY_API_VERSION = "2026-01-22 00:06 UTC"
+DEPLOY_API_VERSION = "2026-01-22 06:52 UTC"
 # =============================================================================
 
 import os
